df753e2dffbc171edf103f2ffa9c04d097a668cb
Moxiner <2665348076@qq.com>
2025-02-01 13:35:36 +0800

    fix: 修复没有管理员权限就闪退的Bug feat: 优化“加速按钮”的显示

--- clean.py
+++ clean.py
@@ -2,7 +2,15 @@ from PyQt5.QtCore import Qt, pyqtSignal, QThread
 from PyQt5.QtGui import QPixmap, QPainter, QColor
 from PyQt5.QtWidgets import QWidget
 
-from qfluentwidgets import FluentIcon as FIF,InfoBar,InfoBarPosition, InfoBarIcon, TeachingTip, TeachingTipTailPosition, Dialog
+from qfluentwidgets import (
+    FluentIcon as FIF,
+    InfoBar,
+    InfoBarPosition,
+    InfoBarIcon,
+    TeachingTip,
+    TeachingTipTailPosition,
+    Dialog,
+)
 from mian_ui_ui import Ui_Form
 
 import json
@@ -11,6 +19,7 @@ import shutil
 import psutil
 import subprocess
 import ctypes
+
 try:
     from plyer import notification
 except Exception as e:
@@ -19,6 +28,7 @@ from datetime import datetime
 import time
 
 import os
+
 # 导入日志模块
 from logger import get_logger
 
@@ -28,10 +38,18 @@ logger = get_logger()
 current_file = "清理内存和临时文件，减少电脑卡顿"
 # 获取当前脚本的目录
 script_directory = os.path.dirname(os.path.abspath(__file__))
-settings_path = f'{script_directory}\WCMain\settings.json'
+settings_path = f"{script_directory}\WCMain\settings.json"
 
-with open(settings_path, 'r') as f:
+with open(settings_path, "r") as f:
     settings_data = json.load(f)
+
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
 
 def get_drive_info(drive_letter):
     partitions = psutil.disk_partitions()
@@ -52,10 +70,11 @@ def get_drive_info(drive_letter):
                 print(f"获取磁盘使用情况时发生错误: {e}")
                 logger.error(f"获取磁盘使用情况时发生错误: {e}")
 
+
 def WeatherLate(date_str):
     now = datetime.now()
     try:
-        specified_date = datetime.strptime(date_str, '%Y-%m-%d')
+        specified_date = datetime.strptime(date_str, "%Y-%m-%d")
         date_difference = now - specified_date
         days_difference = date_difference.days
         return days_difference
@@ -64,29 +83,31 @@ def WeatherLate(date_str):
         logger.error(f"日期格式错误: {e}")
         return None
 
+
 def get_v():
     # 获取C盘的使用情况
-    usage = psutil.disk_usage('C:\\')  # 请注意在Windows上使用双反斜杠来表示路径
+    usage = psutil.disk_usage("C:\\")  # 请注意在Windows上使用双反斜杠来表示路径
 
     # 获取已用空间（以MB为单位）
-    used_space_mb = round(usage.used / (1024 ** 2), 2)  # 转换为MB
+    used_space_mb = round(usage.used / (1024**2), 2)  # 转换为MB
     print(f"已用空间: {used_space_mb} MB")
     return used_space_mb
 
 
 def boost_main():
-    boost_prefetch('C:\\Windows\\Prefetch')
+    boost_prefetch("C:\\Windows\\Prefetch")
     clean_temp_folder()
     clean_system_logs()
     clean_browser_cache()
 
+
 def clean_main():
     global settings_data
-    boost_prefetch('C:\\Windows\\SoftwareDistribution\\Download')
-    boost_prefetch('C:\\Windows\\Prefetch')
-    boost_prefetch('C:\\Windows\\Temp')
-    boost_prefetch('C:\\Windows\\System32\\LogFiles')
-    boost_prefetch('C:\\Windows\\System32\\DriverStore\\FileRepository')
+    boost_prefetch("C:\\Windows\\SoftwareDistribution\\Download")
+    boost_prefetch("C:\\Windows\\Prefetch")
+    boost_prefetch("C:\\Windows\\Temp")
+    boost_prefetch("C:\\Windows\\System32\\LogFiles")
+    boost_prefetch("C:\\Windows\\System32\\DriverStore\\FileRepository")
     clean_temp_folder()
     clean_system_logs()
     clean_application_cache()
@@ -101,18 +122,19 @@ def clean_main():
     for path in user_list:
         boost_prefetch(path)
 
+
 def clean_application_cache():
     # 获取当前用户的用户名
     username = os.getlogin()
     # 构建应用程序缓存目录的路径
-    cache_dir = f'C:\\Users\\{username}\\AppData\\Local\\Packages'
+    cache_dir = f"C:\\Users\\{username}\\AppData\\Local\\Packages"
     # 遍历缓存目录下的所有文件夹
     for root, dirs, files in os.walk(cache_dir):
         for dir in dirs:
             # 构建每个应用程序缓存文件夹的路径
             app_cache_dir = os.path.join(root, dir)
             # 如果文件夹名称以"Cache"或"cache"结尾，则删除该文件夹
-            if dir.lower().endswith('cache'):
+            if dir.lower().endswith("cache"):
                 try:
                     shutil.rmtree(app_cache_dir)
                     print(f"Deleted {app_cache_dir}")
@@ -127,8 +149,10 @@ def clean_tmp_files():
     # 定义要遍历的文件夹路径
     folder_path = "C:\\"  # C盘根目录
     # 获取管理员权限
-    if not os.access(folder_path, os.W_OK):
-        raise PermissionError("You don't have permission to delete files in this folder.")
+    if not is_admin():
+        raise PermissionError(
+            "You don't have permission to delete files in this folder."
+        )
     # 遍历文件夹及其子文件夹
     for root, dirs, files in os.walk(folder_path):
         for file in files:
@@ -142,7 +166,8 @@ def clean_tmp_files():
                     os.remove(file_path)
                     print(f"已删除：{file_path}")
                 except Exception as e:
-                    print(f"Failed to delete: {file_path}, Error: {e}")    
+                    print(f"Failed to delete: {file_path}, Error: {e}")
+
 
 def boost_prefetch(folder_path):
     global current_file
@@ -153,8 +178,10 @@ def boost_prefetch(folder_path):
         return
 
     # 获取管理员权限
-    if not os.access(folder_path, os.W_OK):
-        raise PermissionError("You don't have permission to delete files in this folder.")
+    if not is_admin():
+        raise PermissionError(
+            "You don't have permission to delete files in this folder."
+        )
 
     # 遍历文件夹并删除文件
     for filename in os.listdir(folder_path):
@@ -169,14 +196,20 @@ def boost_prefetch(folder_path):
         except Exception as e:
             print(f"Failed to delete: {file_path}, Error: {e}")
 
+
 def clean_temp_folder():
-    temp_folder = os.environ.get('TEMP')
+    temp_folder = os.environ.get("TEMP")
     boost_prefetch(temp_folder)
+
 
 def clean_browser_cache():
     browser_cache_folders = {
-        "Chrome": os.path.join(os.getenv("LOCALAPPDATA"), "Google\\Chrome\\User Data\\Default\\Cache"),
-        "Edge": os.path.join(os.getenv("LOCALAPPDATA"), "Microsoft\\Edge\\User Data\\Default\\Cache")
+        "Chrome": os.path.join(
+            os.getenv("LOCALAPPDATA"), "Google\\Chrome\\User Data\\Default\\Cache"
+        ),
+        "Edge": os.path.join(
+            os.getenv("LOCALAPPDATA"), "Microsoft\\Edge\\User Data\\Default\\Cache"
+        ),
     }
     print(browser_cache_folders)
 
@@ -185,11 +218,13 @@ def clean_browser_cache():
         logger.info(f"清理 {browser} 浏览器缓存：{folder}")
         boost_prefetch(folder)
 
+
 def clean_system_logs():
     log_folder = os.path.join(os.getenv("SystemRoot"), "Logs")
     print(f"清理系统日志文件夹：{log_folder}")
     logger.info(f"清理系统日志文件夹：{log_folder}")
     boost_prefetch(log_folder)
+
 
 # 删除所有还原点的函数
 def delete_restore_points():
@@ -206,20 +241,31 @@ def delete_restore_points():
 
 class CleanThread(QThread):
     operationCompleted = pyqtSignal()
+    operationFailed_permissionError = pyqtSignal()
 
     def run(self):
-        clean_main()
-        self.operationCompleted.emit()
+        try:
+            clean_main()
+            self.operationCompleted.emit()
+        except PermissionError as e:
+            self.operationFailed_permissionError.emit()
+
 
 class BoostThread(QThread):
     operationCompleted = pyqtSignal()
+    operationFailed_permissionError = pyqtSignal()
 
     def run(self):
-        boost_main()
-        self.operationCompleted.emit()
+        try:
+            boost_main()
+            self.operationCompleted.emit()
+        except PermissionError as e:
+            self.operationFailed_permissionError.emit()
+
 
 class FlashThread(QThread):
     operationCompleted = pyqtSignal(int)
+    operationFailed_permissionError = pyqtSignal()
 
     def run(self):
         while True:
@@ -227,6 +273,7 @@ class FlashThread(QThread):
             memory = int(memory_info.percent)
             self.operationCompleted.emit(memory)
             time.sleep(2)
+
 
 class clean_page(QWidget, Ui_Form):
     def __init__(self, parent=None):
@@ -245,7 +292,7 @@ class clean_page(QWidget, Ui_Form):
         self.progressBar.setTextVisible(True)
 
         self.pushButton.clicked.connect(self.boost)
-        self.widget_2.clicked.connect(self.clean) 
+        self.widget_2.clicked.connect(self.clean)
         self.widget_3.clicked.connect(self.RunSpaceSniffer)
         self.widget_4.clicked.connect(self.RunAppDataCleaner)
 
@@ -254,26 +301,33 @@ class clean_page(QWidget, Ui_Form):
 
     def RunSpaceSniffer(self):
         import subprocess
+
         exe_path = "WCMain/SpaceSniffer.exe"
         process = subprocess.Popen([exe_path])
 
     def RunAppDataCleaner(self):
         import subprocess
+
         exe_path = "WCMain/AppDataCleaner.exe"
-        process = subprocess.Popen([exe_path])    
+        process = subprocess.Popen([exe_path])
 
     def on_operation_completed(self):
+        self.info_bar()
         print("完成")
+
+    def on_operation_failed_PermissionError(self):
+        self.PermissionError_bar()
+        self.pushButton.setEnabled(True)
+        self.widget_2.setEnabled(True)
 
     def run_flash(self):
         self.thread = FlashThread()
         self.thread.operationCompleted.connect(self.update_ui)
-        self.thread.finished.connect(self.on_operation_completed)
         self.thread.start()
 
     def update_ui(self, index):
         global current_file
-        self.progressBar.setValue(index)   
+        self.progressBar.setValue(index)
         file_name = os.path.basename(current_file)
         self.label_3.setText(f"{file_name}")
 
@@ -282,10 +336,12 @@ class clean_page(QWidget, Ui_Form):
         self.v0 = get_v()
         self.pushButton.setEnabled(False)
         self.widget_2.setEnabled(False)
-        self.info_bar()
+        self.pushButton.setText("优化加速中...")
         self.thread = BoostThread()
-        self.thread.operationCompleted.connect(self.on_operation_completed)
-        self.thread.finished.connect(self.after_clean)
+        self.thread.operationCompleted.connect(self.after_clean)
+        self.thread.operationFailed_permissionError.connect(
+            self.on_operation_failed_PermissionError
+        )
         self.thread.start()
 
     def clean(self):
@@ -293,31 +349,44 @@ class clean_page(QWidget, Ui_Form):
         self.v0 = get_v()
         self.pushButton.setEnabled(False)
         self.widget_2.setEnabled(False)
-        self.info_bar()
+        self.pushButton.setText("深度清理中...")
         self.thread = CleanThread()
-        self.thread.operationCompleted.connect(self.on_operation_completed)
-        self.thread.finished.connect(self.deep_after_clean)
+        self.thread.operationCompleted.connect(self.deep_after_clean)
+        self.thread.operationFailed_permissionError.connect(
+            self.on_operation_failed_PermissionError
+        )
         self.thread.start()
 
     def show_work(self):
-        drive_letter = 'C:'
+        drive_letter = "C:"
         info = get_drive_info(drive_letter)
         if info:
             total, used, free, percent = info
-            #print(f"C 盘总空间: {total / (1024 ** 3):.2f} GB")
-            #print(f"C 盘已用空间: {used / (1024 ** 3):.2f} GB")
-            self.label_9.setText(f"{free / (1024 ** 3):.2f}GB可用，共{total / (1024 ** 3):.2f}GB")
-            #print(f"C 盘使用百分比: {percent}%")
+            # print(f"C 盘总空间: {total / (1024 ** 3):.2f} GB")
+            # print(f"C 盘已用空间: {used / (1024 ** 3):.2f} GB")
+            self.label_9.setText(
+                f"{free / (1024 ** 3):.2f}GB可用，共{total / (1024 ** 3):.2f}GB"
+            )
+            # print(f"C 盘使用百分比: {percent}%")
         else:
             print(f"找不到驱动器 {drive_letter}")
             logger.error(f"找不到驱动器 {drive_letter}")
 
+    def clear_failed(self):
+        self.pushButton.setEnabled(True)
+        self.widget_2.setEnabled(True)
+        # self.showTeachingTip(content="清理失败，请重试！")
+
     def after_clean(self):
+        self.pushButton.setText("立即加速")
+        self.info_bar()
         global current_file
         current_file = "清理内存和临时文件，减少电脑卡顿"
         self.v1 = get_v()
-        if int(self.v0-self.v1) > 1024:
-            message = f"加速完成！\n清理出{format((self.v0 - self.v1)/1024, '.2f')}GB空间"
+        if int(self.v0 - self.v1) > 1024:
+            message = (
+                f"加速完成！\n清理出{format((self.v0 - self.v1)/1024, '.2f')}GB空间"
+            )
             logger.info(message)
         else:
             message = f"加速完成！\n清理出{format(self.v0 - self.v1, '.2f')}MB空间"
@@ -328,11 +397,15 @@ class clean_page(QWidget, Ui_Form):
         self.show_work()
 
     def deep_after_clean(self):
+        self.pushButton.setText("立即加速")
+        self.info_bar()
         global current_file
         current_file = "清理内存和临时文件，减少电脑卡顿"
         self.v1 = get_v()
-        if int(self.v0-self.v1) > 1024:
-            message = f"加速完成！\n清理出{format((self.v0 - self.v1)/1024, '.2f')}GB空间"
+        if int(self.v0 - self.v1) > 1024:
+            message = (
+                f"加速完成！\n清理出{format((self.v0 - self.v1)/1024, '.2f')}GB空间"
+            )
             logger.info(message)
         else:
             message = f"加速完成！\n清理出{format(self.v0 - self.v1, '.2f')}MB空间"
@@ -353,37 +426,56 @@ class clean_page(QWidget, Ui_Form):
         else:
             print("Cancel")
 
-        w = Dialog("是否继续清理？", f"是否从'存储'删除临时文件？\n注意：此操作可能并不适用于所有Windows版本。", self)
+        w = Dialog(
+            "是否继续清理？",
+            f"是否从'存储'删除临时文件？\n注意：此操作可能并不适用于所有Windows版本。",
+            self,
+        )
         w.yesButton.setText(f"是(Y)")
         w.cancelButton.setText(f"否(C)")
         if w.exec():
             try:
-                os.popen('start ms-settings:storagesense')
+                os.popen("start ms-settings:storagesense")
             except Exception as e:
                 print(f"error{e}")
         else:
-            print("Cancel")        
-        
+            print("Cancel")
+
     def showTeachingTip(self, content="设置成功！重启软件后生效。"):
         TeachingTip.create(
             target=self.widget,
             icon=InfoBarIcon.SUCCESS,
-            title='Success',
+            title="Success",
             content=content,
             isClosable=True,
             tailPosition=TeachingTipTailPosition.TOP,
             duration=2000,
-            parent=self
-        )         
+            parent=self,
+        )
 
     def info_bar(self):
         InfoBar.success(
-        title='success',
-        content="清理中，请稍后...",
-        orient=Qt.Horizontal,
-        isClosable=True,
-        position=InfoBarPosition.TOP,
-        duration=2000,
-        parent=self
+            title="success",
+            content="清理中，请稍后...",
+            orient=Qt.Horizontal,
+            isClosable=True,
+            position=InfoBarPosition.TOP,
+            duration=2000,
+            parent=self,
         )
-         
+
+    def PermissionError_bar(self):
+        InfoBar.warning(
+            title="没有权限删除此文件",
+            content="请以管理员身份运行",
+            orient=Qt.Horizontal,
+            isClosable=True,
+            position=InfoBarPosition.TOP,
+            duration=5000,
+            parent=self,
+        )
+
+
+if __name__ == "__main__":
+    logger.error("请运行 main.py ，而不是 clean.py")
+    raise RuntimeError("请运行 main.py ，而不是 clean.py")
