05da4855303aa80649e05fcd1376e9aa70ffc746
darkmatter2048 <darkmatter2048@outlook.com>
2025-06-18 20:53:07 +0800

    修改UI，开始准备国际化

---
+++ cn_en.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="senior_ui_ui.py" line="89"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="senior_ui_ui.py" line="90"/>
+        <source>移除系统组件或修改系统设置Beta</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="senior_ui_ui.py" line="91"/>
+        <source>执行</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="senior_ui_ui.py" line="92"/>
+        <source>转移虚拟内存</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="senior_ui_ui.py" line="93"/>
+        <source>注意⚠：此操作可能会导致
+Steam,VMware等软件无法正常运行</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="senior_ui_ui.py" line="95"/>
+        <source>关闭系统休眠</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="senior_ui_ui.py" line="96"/>
+        <source>✅关闭后不会影响系统睡眠及正常使用</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
