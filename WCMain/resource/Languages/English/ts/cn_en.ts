d0b34e123b36dd1b02c8721caecfaf70850b7e02
darkmatter2048 <darkmatter2048@outlook.com>
2025-06-20 17:28:50 +0800

    EN语言包

--- WCMain/resource/Languages/English/ts/cn_en.ts
+++ WCMain/resource/Languages/English/ts/cn_en.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="en" sourcelanguage="ab">
 <context>
     <name>Form</name>
     <message>
@@ -11,33 +11,39 @@
     <message>
         <location filename="senior_ui_ui.py" line="90"/>
         <source>移除系统组件或修改系统设置Beta</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>Remove system components or modify system settings Beta</translatorcomment>
+        <translation>Remove system components or modify system settings Beta</translation>
     </message>
     <message>
         <location filename="senior_ui_ui.py" line="91"/>
         <source>执行</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>Run</translatorcomment>
+        <translation>Run</translation>
     </message>
     <message>
         <location filename="senior_ui_ui.py" line="92"/>
         <source>转移虚拟内存</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>transfer virtual memory</translatorcomment>
+        <translation>Transfer virtual memory</translation>
     </message>
     <message>
         <location filename="senior_ui_ui.py" line="93"/>
         <source>注意⚠：此操作可能会导致
 Steam,VMware等软件无法正常运行</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>Note: This operation may cause Steam, VMware and other software to not run properly.</translatorcomment>
+        <translation>Note: This operation may cause \n Steam, VMware and other software to not run properly</translation>
     </message>
     <message>
         <location filename="senior_ui_ui.py" line="95"/>
         <source>关闭系统休眠</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>Turn off system hibernation</translatorcomment>
+        <translation>Turn off system hibernation</translation>
     </message>
     <message>
         <location filename="senior_ui_ui.py" line="96"/>
         <source>✅关闭后不会影响系统睡眠及正常使用</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>✅After closing, it will not affect the system sleep and normal use</translatorcomment>
+        <translation>✅After closing, it will not affect the system sleep and normal use</translation>
     </message>
 </context>
 </TS>
