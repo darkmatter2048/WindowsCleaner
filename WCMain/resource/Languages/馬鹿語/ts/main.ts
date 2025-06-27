6b3a5bf2a0a75a3f122bb99ed98d1bdda7b5545d
darkmatter2048 <darkmatter2048@outlook.com>
2025-06-27 20:53:13 +0800

    上传馬鹿語模板

---
+++ WCMain/resource/Languages/馬鹿語/ts/main.ts
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en" sourcelanguage="zh_CN">
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="main_ui_ui.py" line="276"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui_ui.py" line="277"/>
+        <source>君子喻于义，小人喻于利。</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui_ui.py" line="278"/>
+        <source>一键加速</source>
+        <translatorcomment>One-Click Boost</translatorcomment>
+        <translation>One-Click Boost</translation>
+    </message>
+    <message>
+        <location filename="main_ui_ui.py" line="279"/>
+        <source>立即加速</source>
+        <translatorcomment>Boost Now</translatorcomment>
+        <translation>Boost Now</translation>
+    </message>
+    <message>
+        <location filename="main_ui_ui.py" line="280"/>
+        <source>内存占用</source>
+        <translatorcomment>Memory Usage</translatorcomment>
+        <translation>Memory Usage</translation>
+    </message>
+    <message>
+        <location filename="main_ui_ui.py" line="281"/>
+        <source>清理内存和临时文件，减少电脑卡顿</source>
+        <translatorcomment>Clean up memory and temporary files to reduce computer lag</translatorcomment>
+        <translation>Clean up memory and temporary files to reduce computer lag</translation>
+    </message>
+    <message>
+        <location filename="main_ui_ui.py" line="282"/>
+        <source>深度清理</source>
+        <translatorcomment>Deep Clean</translatorcomment>
+        <translation>Deep Clean</translation>
+    </message>
+    <message>
+        <location filename="main_ui_ui.py" line="283"/>
+        <source>本地磁盘(C:)</source>
+        <translatorcomment>Local Disk (C:)</translatorcomment>
+        <translation>Local Disk (C:)</translation>
+    </message>
+    <message>
+        <location filename="main_ui_ui.py" line="284"/>
+        <source>1024G/10TB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui_ui.py" line="285"/>
+        <source>磁盘分析</source>
+        <translatorcomment>Disk Analysis</translatorcomment>
+        <translation>Disk Analysis</translation>
+    </message>
+    <message>
+        <location filename="main_ui_ui.py" line="286"/>
+        <source>SpaceSniffer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui_ui.py" line="287"/>
+        <source>（请谨慎使用）</source>
+        <translatorcomment>(Use with caution)</translatorcomment>
+        <translation>(Use with caution)</translation>
+    </message>
+    <message>
+        <location filename="main_ui_ui.py" line="288"/>
+        <source>数据迁移</source>
+        <translatorcomment>Data Migration</translatorcomment>
+        <translation>Data Migration</translation>
+    </message>
+    <message>
+        <location filename="main_ui_ui.py" line="289"/>
+        <source>移动AppData文件夹</source>
+        <translatorcomment>Move AppData Folder</translatorcomment>
+        <translation>Move AppData Folder</translation>
+    </message>
+    <message>
+        <location filename="main_ui_ui.py" line="293"/>
+        <source>（请谨慎清理）</source>
+        <translatorcomment>(Clean with caution)</translatorcomment>
+        <translation>(Clean with caution)</translation>
+    </message>
+    <message>
+        <location filename="main_ui_ui.py" line="291"/>
+        <source>高级选项</source>
+        <translatorcomment>Advanced Options</translatorcomment>
+        <translation>Advanced Options</translation>
+    </message>
+    <message>
+        <location filename="main_ui_ui.py" line="292"/>
+        <source>清理部分系统组件</source>
+        <translatorcomment>Clean Some System Components</translatorcomment>
+        <translation>Clean Some System Components</translation>
+    </message>
+    <message>
+        <location filename="main_ui_ui.py" line="294"/>
+        <source>Copyright (C) 2021-2025.DaYe</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
